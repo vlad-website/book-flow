214a9ee53515704dc4432a790ebfba1aa15d8d7f
KharkovskyiVladyslav <vlad.website33@gmail.com>
2026-05-06 08:22:12 +0200

    feat(auth): add current user decorator

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -12,6 +12,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return payload;
+    return {
+      userId: payload.userId,
+      role: payload.role,
+    };
   }
 }
